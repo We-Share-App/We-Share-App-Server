5b5b3d8b3b9b10cdc6a327b3666656e1636e0f7d
bell-person-ii <leeji88640@gmail.com>
2025-07-03 01:47:04 +0900

    JWTUtil.java 생성

---
+++ src/main/java/com/weshare/server/user/jwt/JWTUtil.java
@@ -0,0 +1,56 @@
+package com.weshare.server.user.jwt;
+
+import com.weshare.server.user.entity.UserRole;
+import io.jsonwebtoken.Jwts;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+
+@Component
+// Access/Refresh 토큰 유틸리티 클래스
+public class JWTUtil {
+    private final SecretKey secretKey;
+
+    public JWTUtil(@Value("${spring.jwt.secret}")String secret) {
+        secretKey = new SecretKeySpec(secret.getBytes(StandardCharsets.UTF_8), Jwts.SIG.HS256.key().build().getAlgorithm());
+    }
+
+    // 토큰 -> Username 추출 메서드
+    public String getUsername(String token) {
+
+        return Jwts.parser().verifyWith(secretKey).build().parseSignedClaims(token).getPayload().get("username", String.class);
+    }
+
+    // 토큰 -> UserRole 추출 메서드
+    public String getRole(String token) {
+
+        return Jwts.parser().verifyWith(secretKey).build().parseSignedClaims(token).getPayload().get("role", String.class);
+    }
+
+    // 토큰 -> 토큰종류(Access/Refresh) 추출 메서드
+    public String getCategory(String token){
+        return Jwts.parser().verifyWith(secretKey).build().parseSignedClaims(token).getPayload().get("category", String.class);
+    }
+
+    // 토큰 -> 만료여부 리턴 메서드
+    public Boolean isExpired(String token) {
+
+        return Jwts.parser().verifyWith(secretKey).build().parseSignedClaims(token).getPayload().getExpiration().before(new Date());
+    }
+
+    public String createJwt(String category, String username, UserRole userRole, Long expiredMs) {
+
+        return Jwts.builder()
+                .claim("category",category)
+                .claim("username", username)
+                .claim("role", userRole.getRoleName())
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() + expiredMs))
+                .signWith(secretKey)
+                .compact();
+    }
+}
