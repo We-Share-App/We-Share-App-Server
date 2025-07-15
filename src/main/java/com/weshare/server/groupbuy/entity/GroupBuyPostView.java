70e4e4529150b0b0605d77a66567aec789db6271
bell-person-ii <leeji88460@gmail.com>
2025-07-15 21:58:18 +0900

    GroupBuyPostView 엔티티 생성

--- src/main/java/com/weshare/server/groupbuy/entity/GroupBuyPostView.java
+++ src/main/java/com/weshare/server/groupbuy/entity/GroupBuyPostView.java
@@ -1,23 +1,25 @@
 package com.weshare.server.groupbuy.entity;
 
 import com.weshare.server.common.entity.BaseTimeEntity;
+import com.weshare.server.user.entity.User;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
-@Table(name = "group_buy_post_image")
+@Table(name = "group_buy_post_view")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
-public class GroupBuyPostImage extends BaseTimeEntity {
+public class GroupBuyPostView extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "group_buy_post_image_key")
-    private String groupBuyPostImageKey;
+    @ManyToOne(fetch = FetchType.LAZY )
+    @JoinColumn(name = "user_id")
+    private User user;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "group_buy_post_id")
