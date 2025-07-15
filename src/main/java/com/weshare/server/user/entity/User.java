33d04356cb975dd3028377b33deb7539fdda4a84
bell-person-ii <leeji88460@gmail.com>
2025-07-15 19:18:44 +0900

    User 엔티티 설정 완료

--- src/main/java/com/weshare/server/user/entity/User.java
+++ src/main/java/com/weshare/server/user/entity/User.java
@@ -1,10 +1,7 @@
 package com.weshare.server.user.entity;
 
 import com.weshare.server.common.entity.BaseTimeEntity;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -18,10 +15,18 @@ public class User extends BaseTimeEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false, unique = true, length = 20)
     private String username; //  서비스내에서의 사용자 이름
+
+    @Column(nullable = false, length = 20)
     private String name; // 소셜 등록 사용자 이름
+
+    @Column(nullable = false, unique = true)
     private String email; // 사용자 이메일
+
     private String phoneNumber; // 사용자 전화번호
+
+    @Enumerated(EnumType.STRING)
     private UserRole userRole; // 사용자 구분
 
     public User(String username,String name, String email, UserRole userRole){
