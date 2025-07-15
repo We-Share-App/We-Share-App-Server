a8ef1fb792cb68aa3262ddb0cb31f0f26e890842
bell-person-ii <leeji88460@gmail.com>
2025-07-16 02:17:24 +0900

    ExchangeProposalPostImage 엔티티 생성

---
+++ src/main/java/com/weshare/server/exchange/proposal/entity/ExchangeProposalPostImage.java
@@ -0,0 +1,26 @@
+package com.weshare.server.exchange.proposal.entity;
+
+import com.weshare.server.common.entity.BaseTimeEntity;
+import com.weshare.server.exchange.entity.ExchangePost;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "exchange_proposal_post_image")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class ExchangeProposalPostImage extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "exchange_proposal_post_image_key", nullable = false)
+    private String exchangeProposalPostImageKey;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "exchange_proposal_post_id", nullable = false)
+    private ExchangeProposalPost exchangeProposalPost;
+
+}
