21fee75e8ab972a9f8f3080045d8e94b893d6a2c
bell-person-ii <leeji88460@gmail.com>
2025-07-16 00:52:16 +0900

    GroupBuyChatRoom 엔티티 생성

---
+++ src/main/java/com/weshare/server/groupbuy/chat/entity/GroupBuyChatRoom.java
@@ -0,0 +1,22 @@
+package com.weshare.server.groupbuy.chat.entity;
+
+import com.weshare.server.common.entity.BaseTimeEntity;
+import com.weshare.server.groupbuy.entity.GroupBuyPost;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "group_buy_group_chat_room")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class GroupBuyChatRoom extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "group_buy_post_id", nullable = false)
+    private GroupBuyPost groupBuyPost;
+}
