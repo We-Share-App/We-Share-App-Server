367a6b6afbc633fd483602db5b999936bab6b70c
bell-person-ii <leeji88460@gmail.com>
2025-07-16 01:28:10 +0900

    ExchangePost 엔티티 생성

---
+++ src/main/java/com/weshare/server/exchange/entity/ExchangePost.java
@@ -0,0 +1,45 @@
+package com.weshare.server.exchange.entity;
+
+import com.weshare.server.common.entity.BaseTimeEntity;
+import com.weshare.server.exchange.ItemCondition;
+import com.weshare.server.location.entity.Location;
+import com.weshare.server.user.entity.User;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "exchange_post")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class ExchangePost extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 50)
+    private String itemName;
+
+    @Column(nullable = false)
+    private String itemDescription;
+
+    @Column(nullable = false)
+    private LocalDateTime recruitingExpirationDate;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private ItemCondition itemCondition;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne(fetch =  FetchType.LAZY)
+    @JoinColumn(name = "location_id", nullable = false)
+    private Location location;
+
+
+}
