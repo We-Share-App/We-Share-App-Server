6b029b5035777d278324cc4ef4e411da7cb08fcf
bell-person-ii <leeji88460@gmail.com>
2025-07-18 18:08:07 +0900

    Refresh 엔티티 Table명 선언

--- src/main/java/com/weshare/server/user/entity/Refresh.java
+++ src/main/java/com/weshare/server/user/entity/Refresh.java
@@ -1,15 +1,13 @@
 package com.weshare.server.user.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
+@Table(name = "refresh")
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Refresh {
